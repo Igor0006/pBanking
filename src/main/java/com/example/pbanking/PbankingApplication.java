8a05add7f91626a3d23416a2a98db8324ef6b127
Igor0006 <igordubrovskij69@gmail.com>
2025-10-29 15:05:14 +0300

    initital commit

---
+++ src/main/java/com/example/pbanking/PbankingApplication.java
@@ -0,0 +1,13 @@
+package com.example.pbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pbanking/PbankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
